3cb63736b3fc19ec5668eebee5c793420aa48601
HazalCevikk <hazal.cevikk@gmail.com>
2024-07-02 09:56:17 +0100

    feat: Add Header component with logo, user info, and logout functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import CabinetAssemblyProcess from "@/components/CabinetAssemblyProcess";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header user={"Richard Clarkson"}></Header>
+      <CabinetAssemblyProcess/>
+    </>
   );
 }
